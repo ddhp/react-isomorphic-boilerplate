3db6888f49e114e772353171c17f0443570f376f
ddhp <sevenknee@gmail.com>
2017-11-22 15:28:44 +0800

    home reducer test

---
+++ src/reducers/pages/home.spec.js
@@ -0,0 +1,28 @@
+import test from 'ava';
+import sinon from 'sinon';
+
+import reducer from './home';
+
+test('ADD_POST', t => {
+  const initState = {
+    posts: []
+  };
+  const action = {
+    type: 'ADD_POST',
+    payload: {
+      result: 1
+    }
+  };
+  t.deepEqual(reducer(initState, action), {
+    posts: [1]
+  });
+
+  // return original state if no response
+  const wrongAction = {
+    type: 'ADD_POST',
+    payload: {
+      result: undefined
+    }
+  }
+  t.is(reducer(initState, wrongAction), initState);
+});
