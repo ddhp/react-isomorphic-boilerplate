17a39791952eb8349c22def7fe667bf916b58cbf
ddhp <sevenknee@gmail.com>
2017-12-07 16:01:53 +0800

    export default schema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,3 +1,7 @@
 import { schema } from 'normalizr';
 
 export const post = new schema.Entity('posts');
+
+// you need to export a default module
+// or it would be undefined when import module from 'path/to/module'
+export default module.exports;
