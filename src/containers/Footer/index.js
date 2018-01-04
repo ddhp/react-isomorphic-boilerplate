732c3559a649f6dbbd102eaeaf4982cea6946bad
JuinHui Fu <fjh8079@gmail.com>
2018-01-04 20:26:33 +0800

    fix eslint - Component should be written as a pure function

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,13 +1,10 @@
-/* eslint class-methods-use-this: 0 */
 import React from 'react';
 import './style.scss';
 
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <a href="https://github.com/ddhp/react-isomorphic-boilerplate" target="_blank" rel="noopener noreferrer">react isomorphic boilerplate</a> by ddhp
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div className="footer">
+    <a href="https://github.com/ddhp/react-isomorphic-boilerplate" target="_blank" rel="noopener noreferrer">react isomorphic boilerplate</a> by ddhp
+  </div>
+);
+
+export default Footer;
--- src/containers/Nav/index.js
+++ src/containers/Nav/index.js
@@ -5,26 +5,26 @@ import { get as _get } from 'lodash';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-export class Nav extends React.Component {
-  static propTypes = {
-    name: PropTypes.string,
-  }
+const Nav = ({ name }) => (
+  <nav>
+    <Link to="/">Rib.</Link>
+    <Link to="/about">About</Link>
+    <Link to="/demo">Demo</Link>
+    <a href="another-entry">Another</a>
 
-  render() {
-    return (
-      <nav>
-        <Link to="/">Rib.</Link>
-        <Link to="/about">About</Link>
-        <Link to="/demo">Demo</Link>
-        <a href="another-entry">Another</a>
+    <div className="user-info">
+      your are {name}
+    </div>
+  </nav>
+);
 
-        <div className="user-info">
-          your are {this.props.name}
-        </div>
-      </nav>
-    );
-  }
-}
+Nav.propTypes = {
+  name: PropTypes.string,
+};
+
+Nav.defaultProps = {
+  name: '',
+};
 
 function mapStateToProps(state) {
   const entities = _get(state, 'entities');
