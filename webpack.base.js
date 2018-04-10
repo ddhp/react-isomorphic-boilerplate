01eed69be6728ac6a95b7d39af6d127e380dee04
ddhp <sevenknee@gmail.com>
2018-04-10 16:19:16 +0800

    related assets

--- webpack.base.js
+++ webpack.base.js
@@ -14,6 +14,12 @@ function baseConfig(platform = 'browser', env) {
 
     plugins: [],
 
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+
     module: {
       rules: [
         {
